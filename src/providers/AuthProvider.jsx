e9414947393818abeb295df2aaa46e64d5e36d39
Himu004 <himu.chowdhury01@gmail.com>
2025-01-09 16:01:15 +0600

    Context and AuthProvider Created

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { AuthContext } from "./Context";
+
+
+const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const authInfo = {
+        user,
+        loading,
+    };
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
